b17aa810ef15b128fafb2b96fb6d55e6d52fa22a
lwj <864391385@qq.com>
2018-02-12 15:04:03 +0800

    update：6-1 核心类生成-Mybatis Generator

---
+++ src/main/java/com/lwj/dao/SysAclMapper.java
@@ -0,0 +1,17 @@
+package com.lwj.dao;
+
+import com.lwj.model.SysAcl;
+
+public interface SysAclMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysAcl record);
+
+    int insertSelective(SysAcl record);
+
+    SysAcl selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysAcl record);
+
+    int updateByPrimaryKey(SysAcl record);
+}
---
+++ src/main/java/com/lwj/dao/SysRoleAclMapper.java
@@ -0,0 +1,17 @@
+package com.lwj.dao;
+
+import com.lwj.model.SysRoleAcl;
+
+public interface SysRoleAclMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysRoleAcl record);
+
+    int insertSelective(SysRoleAcl record);
+
+    SysRoleAcl selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysRoleAcl record);
+
+    int updateByPrimaryKey(SysRoleAcl record);
+}
